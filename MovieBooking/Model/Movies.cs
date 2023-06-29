a28e6b0e42dfb150905515ff97e76f4c4048fc46
Kumudhini S S <Kumudhini.SS@ntgfreight.com>
2023-06-29 12:24:35 +0530

    movie

--- MovieBooking/Model/Movies.cs
+++ MovieBooking/Model/Movies.cs
@@ -9,19 +9,14 @@ namespace MovieBooking.Model
         [BsonRepresentation(BsonType.ObjectId)]
         public string? _id { get; private set; }
 
-        [BsonElement("moviename")]
         public string MovieName { get; set; } = String.Empty;
 
-        [BsonElement("totaltickets")]
         public int TotalNumberOfTickets { get; set; }
 
-        [BsonElement("theatrename")]
         public string TheatreName { get; set; } = String.Empty;
 
-        [BsonElement("moviestatus")]
         public string MovieStatus { get; set; } = String.Empty;
 
-        [BsonElement("ticketremaining")]
         public int TicketsRemaining { get; set; }
     }
 }
